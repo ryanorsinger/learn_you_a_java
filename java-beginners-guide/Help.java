1b702adbe9f39712d5698d9701590c495d6ff65d
ryanorsinger <ryan.orsinger@gmail.com>
2016-06-06 12:45:39 -0500

    Add interactivity to help class.

--- java-beginners-guide/Help.java
+++ java-beginners-guide/Help.java
@@ -28,5 +28,11 @@ class Help {
                 System.out.println("    // etc...");
                 System.out.println("}");
         }
+
+        System.out.println("Press S to stop");
+
+        for(int i = 0; (char) System.in.read() != 'S'; i++) {
+            System.out.println("Pass #" + i);
+        }
     }
 }
