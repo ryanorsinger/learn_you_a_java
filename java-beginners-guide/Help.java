55501b166962deb3265eea5731d5a0ab6410f4ae
ryanorsinger <ryan.orsinger@gmail.com>
2016-06-07 10:33:12 -0500

    Build out STDIN help tool

--- java-beginners-guide/Help.java
+++ java-beginners-guide/Help.java
@@ -7,32 +7,53 @@ class Help {
         System.out.println("Help on:");
         System.out.println("    1. if");
         System.out.println("    2. switch");
-        System.out.println("Choose one: ");
-        char choice;
-        choice = (char) System.in.read();
-
-        switch(choice) {
-            case '1':
-                System.out.println("The if: \n");
-                System.out.println("if(condition) statement;");
-                System.out.println("else statement;");
-                break;
-            case '2':
-                System.out.println("The switch:\n");
-                System.out.println("switch(expression) {");
-                System.out.println("    case constant1:");
-                System.out.println("        statement sequence");
-                System.out.println("        break;");
-                System.out.println("    case constant2");
-                System.out.println("        statement sequence");
-                System.out.println("    // etc...");
-                System.out.println("}");
-        }
-
-        System.out.println("Press S to stop");
-
-        for(int i = 0; (char) System.in.read() != 'S'; i++) {
-            System.out.println("Pass #" + i);
+        System.out.println("    3. for");
+        System.out.println("    4. while");
+        System.out.println("    5. do-while");
+        System.out.println("    6. break");
+        System.out.println("    7. continue"); 
+        System.out.println("\n");
+        System.out.println("Choose one: or q to quit");
+        char choice, ignore;
+        
+        // for loops that look like a cartoon Cthulu are infinite loops to be broken out of later
+        for(;;) {
+        	do {
+        		choice = (char) System.in.read();
+        		
+        		do {
+        			ignore = (char) System.in.read();
+        		} while (ignore != '\n');
+        	} while (choice < '1' | choice > '7'  &  choice != 'q');
+        	
+        	if(choice == 'q') break;
+        	System.out.println("\n");
+	        	
+	        switch(choice) {
+	            case '1':
+	                System.out.println("The if: \n");
+	                System.out.println("if(condition) statement;");
+	                System.out.println("else statement;");
+	                break;
+	            case '2':
+	                System.out.println("The switch:\n");
+	                System.out.println("switch(expression) {");
+	                System.out.println("    case constant1:");
+	                System.out.println("        statement sequence");
+	                System.out.println("        break;");
+	                System.out.println("    case constant2");
+	                System.out.println("        statement sequence");
+	                System.out.println("    // etc...");
+	                System.out.println("}");
+	                break;
+	            case '3':
+	            case '4':
+	            case '5':
+	            case '6':
+	            case '7': 
+	            	System.out.println("section under construction");
+	        }
+	        System.out.println();
         }
     }
 }
